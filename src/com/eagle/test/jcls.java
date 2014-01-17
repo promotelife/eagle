8e50dacc0db96356bf57591a971feae264f21eeb
promotelife <alex.qinyu@gmail.com>
2014-01-17 09:09:23 +0800

    add all

---
+++ src/com/eagle/test/jcls.java
@@ -0,0 +1,17 @@
+package com.eagle.test;
+import org.apache.log4j.Logger;
+public class jcls
+{
+	private Logger log= Logger.getLogger(this.getClass().getName());
+	public void print() {
+		System.out.println("sadf");
+		System.out.println("sadf");
+		System.out.println("sadf");
+		System.out.println("sadf"); 
+	}
+	public void LoggerTest()
+	{
+		log.info("Hi Log,Sublime Test");
+	}
+
+}
