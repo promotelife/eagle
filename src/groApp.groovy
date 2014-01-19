95f7a394d656d0902d6b7c84e5f880625b5d6db9
qinyu <alex.qinyu@foxmail.com>
2014-01-19 22:44:19 +0800

    del iml

---
+++ src/groApp.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created by ALEX on 14-1-19.
+ */
+class groApp {
+    void hello()
+    {
+        println("hello");
+    }
+}
---
+++ src/main.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created by ALEX on 14-1-19.
+ */
+
+app = new groApp()
+app.hello()
+
+ap2 =new Eagle();
+ap2.print();
